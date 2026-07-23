a003f843a63b27a57dc22748aff4e6dbb81554f8
Joanderson Souza <joandersonsouza410@gmail.com>
2026-07-23 18:00:14 -0300

    feat: implement ticket repository methods

--- HelpDeskconsole/Repositories/ChamadoRepository.cs
+++ HelpDeskconsole/Repositories/ChamadoRepository.cs
@@ -5,7 +5,31 @@ namespace HelpDeskconsole.Repositories
     public class ChamadoRepository {
 
         private List<Chamado> _chamados = new();
-    
+
+        public void Cadastrar(Chamado chamado)
+        {
+            _chamados.Add(chamado);
+        }
+
+        public List<Chamado> Listar()
+        {
+            return _chamados;
+        }
+
+        public Chamado? BuscarPorId(int id) { 
+        
+          foreach (var chamado in _chamados) { 
+             
+                if(chamado.Id == id)
+                {
+                    return chamado;
+                }
+           
+            }
+
+            return null; 
+        }
+
     }
 
 }
