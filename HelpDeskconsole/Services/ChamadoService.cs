66f045ba4e8775e776bfd9e7150a1030cb9cb514
Joanderson Souza <joandersonsouza410@gmail.com>
2026-07-24 00:33:50 -0300

    feat: add ticket service and repository integration

---
+++ HelpDeskconsole/Services/ChamadoService.cs
@@ -0,0 +1,19 @@
+﻿using HelpDeskconsole.Repositories;
+using HelpDeskconsole.Models;
+
+namespace HelpDeskconsole.Services
+{
+    public class ChamadoService
+    {
+        private ChamadoRepository _repository = new();
+
+
+        public void AbrirChamado(Chamado chamado)
+        {
+            _repository.Cadastrar(chamado);
+        }
+
+    }
+
+
+}   
